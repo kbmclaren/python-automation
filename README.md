d8ae894271e97a0f771a172cdc83818d6e270b28
Caleb McLaren <mclaren1@umbc.edu>
2022-04-20 15:48:32 -0400

    convert shakeEightBall() to iterative version, update README.md

--- README.md
+++ README.md
@@ -2,15 +2,15 @@
 
 ## Script
 * bulletPointAdder.py: Text in clipboard modified to include leading "* " on each line.
-* characterCount.py: Disply the count of characters seen in message.
+* characterCount.py: Display the count of characters seen in message.
 * exitExample.py: Demonstrate the use of system.exit() via user input.
 * firstProg.py: An extended 'Hello World', taking in user name and age.
 * mclip.py: Usage: python mclip.py [keyphrase] OR ./mclip.py [keyphrase]
 	* myGoodLines.txt: plain text searched by mclip.py for keyphrases.
-* zigzag.py: print out a group of stars in a zig-zag pattern on the command line
+* zigzag.py: print out a group of stars in a zig-zag pattern on the command line.
 
 
 ## Module
-* magicEightBall.py
+* magicEightBall.py: Magic eight ball program for command line execution. Import whole module so shakeEightBall() works.
 * useCollatz.py: TODO: REWRITE SO COLLATZ FUNCTION INDEPENDENT OF WHILE LOOP IN MAIN().
 
--- magicEightBall.py
+++ magicEightBall.py
@@ -1,9 +1,13 @@
-#file: magicEightBall.py
-#author: Caleb M. McLaren
-#email: kbmclaren@gmail.com
-#date: April 6th, 2022
-#description: Magic eight ball program for command line execution.
-#future: convert main from recursive to while loop
+#! python3
+
+"""
+file: magicEightBall.py
+author: Caleb M. McLaren, original by Al Sweigart
+email: kbmclaren@gmail.com
+date: April 6th, 2022
+description: Magic eight ball program for command line execution.
+"""
+
 
 import sys, random, time
 
@@ -19,13 +23,17 @@ messages = ['It is certain',
 
 #name: shakeEightBall
 #input: none
-#return: random answer out of eight possible answers.
+#return: string, random answer out of eight possible answers.
 def shakeEightBall():
 	for i in range(3):
-		#print('.', end='')
 		time.sleep(0.5)
-		print('.', end='')
+		#Python is linebuffered by default. 
+		#Print output is collected in the buffer until a newline is seen, then dumped.
+		#Forcefully flush buffer to see this for loop print a dot, wait, print a dot, wait, etc.
+		print('.', end='', flush=True)
 
+	time.sleep(0.5)
+	print('.', end='', flush=True)
 	print(messages[random.randint(0, len(messages) - 1)])
 	return
 
@@ -34,17 +42,17 @@ def shakeEightBall():
 #return: none
 #description: Request user input, handle user input, call main(), call shakeEightBall(), call sys.exit()
 def main():
-	user_input = input('Will you shake the magic 8 ball? (y/n): ')
+	user_input = ''
+	while( user_input != 'n' ):
+		user_input = input('Will you shake the magic 8 ball? (y/n): ')
+		if( user_input == 'y' ):
+			shakeEightBall()
 	
-	#no input
-	if( user_input == '' ):
-		main()
-	elif( user_input == 'n'):
-		sys.exit(0)
-	else:
-		shakeEightBall()
-		main()
+	sys.exit(0)
+
 
 if __name__ == "__main__":
 	main()
 
+
+# end magicEightBall.py
